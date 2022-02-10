9e1bec67248b64154d075bd143fa05bd1ae59c07
ayyappag <ayyappag@gmail.com>
2022-02-10 10:47:11 +0530

    added

--- src/com/array/LeftRotateArray.java
+++ src/com/array/LeftRotateArray.java
@@ -1,78 +1,35 @@
 package com.array;
 
+import java.util.Arrays;
+
 //Time Complexity : O(n)
 
 public class LeftRotateArray {
 
-	/* Function to left rotate arr[] of size n by d */
-	static void leftRotate(int arr[], int d) {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = { -4, -1, 0, 3, 10 };
+		int k = 1;// number of rotations
+		rotate(a, k);
+		System.out.println(Arrays.toString(a));
 
-		if (d == 0)
-			return;
-
-		int n = arr.length;
-		// in case the rotating factor is
-		// greater than array length
-		d = d % n;
-		reverseArray(arr, 0, d - 1);
-		 printArray(arr);
-		System.out.println("*");
-		reverseArray(arr, d, n - 1);
-		 printArray(arr);
-		System.out.println("**");
-		reverseArray(arr, 0, n - 1);
-		 printArray(arr);
-		System.out.println("***");
 	}
 
-	/* Function to reverse arr[] from index start to end */
-	static void reverseArray(int arr[], int start, int end) {
-		int temp;
-		while (start < end) { // swapping the values
-			temp = arr[start];
-			arr[start] = arr[end];
-			arr[end] = temp;
+	public static void rotate(int[] nums, int k) {
+		k %= nums.length;
+		reverse(nums, 0, nums.length - 1);
+		reverse(nums, 0, k - 1);
+		reverse(nums, k, nums.length - 1);
+	}
+
+	public static void reverse(int[] nums, int start, int end) {
+		while (start < end) {
+			int temp = nums[start];
+			nums[start] = nums[end];
+			nums[end] = temp;
 			start++;
 			end--;
 		}
-	}
-
-	// test
-	static void rotating(int arr[], int rotation) {
-
-		/*
-		 * for (int i = 0, j = arr.length - 1, d = 0; (i < j && d <= rotation); i++,
-		 * j--, d++) { int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
-		 * 
-		 * }
-		 */
-
-		int i = 0, j = arr.length - 1, d = 0;
-		while ((i != j) && (d < 2)) {
-			int temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = temp;
-			i++;d++;
-		}
-	}
-
-	/* UTILITY FUNCTIONS */
-	/* function to print an array */
-	static void printArray(int arr[]) {
-		for (int i = 0; i < arr.length; i++)
-			System.out.print(arr[i] + " ");
-	}
-
-	/* Driver program to test above functions */
-	public static void main(String[] args) {
-		int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
-		int n = arr.length;
-		int d = 2;
-
-		 leftRotate(arr, d); // Rotate array by d -- 3 4 5 6 7 1 2
-
-	//	rotating(arr, d); // -- 7 6 5 4 3 2 1
-		printArray(arr);
 	}
 
 }
